0a83980fc4f48a79fdaf2146af7547ac3e9f6192
3nricomarinelli <enrico.marinelli2910@gmail.com>
2025-01-21 08:52:05 +0100

    Docker for the pi and small changes in ros setup

--- src/sim_mdrs/setup.py
+++ src/sim_mdrs/setup.py
@@ -11,6 +11,8 @@ setup(
     py_modules=[],
     data_files=[
         # Include launch, config, and urdf directories
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
         (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf.xacro')),
@@ -18,11 +20,10 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    author='Your Name',
-    author_email='your_email@example.com',
+    # author='Your Name',
+    # author_email='your_email@example.com',
     description='The sim_mdrs package for robot control',
     license='Apache-2.0',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'keyboard_control = sim_mdrs.keyboard_control:main',
