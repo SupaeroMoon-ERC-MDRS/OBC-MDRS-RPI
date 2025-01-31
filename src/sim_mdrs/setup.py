d7d379a1340b016f32574d5826a30c6e138ac004
eseabro <62852911+eseabro@users.noreply.github.com>
2025-01-31 22:20:09 +0100

    moved roboclaw driver to be executable

--- src/sim_mdrs/setup.py
+++ src/sim_mdrs/setup.py
@@ -19,6 +19,7 @@ setup(
         (os.path.join('share', package_name, 'urdf/meshes'), glob('urdf/meshes/*.stl')),
         (os.path.join('share', package_name, 'urdf/arm_meshes'), glob('urdf/arm_meshes/*.stl')),
         (os.path.join('lib', package_name), glob('sim_mdrs/udpcanpy*')),
+        (os.path.join('lib', package_name), glob('roboclaw/roboclaw_driver.py')),
         (os.path.join('lib', package_name), glob('sim_mdrs/comms.dbc')),
     ],
     install_requires=['setuptools'],
