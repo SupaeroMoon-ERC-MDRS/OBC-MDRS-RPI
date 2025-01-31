008bb6230c8e1f19ea95c0cc01acddb792fffa50
eseabro <62852911+eseabro@users.noreply.github.com>
2025-01-31 18:17:10 +0100

    changed udp in setup

--- src/sim_mdrs/setup.py
+++ src/sim_mdrs/setup.py
@@ -18,7 +18,7 @@ setup(
         (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf.xacro')),
         (os.path.join('share', package_name, 'urdf/meshes'), glob('urdf/meshes/*.stl')),
         (os.path.join('share', package_name, 'urdf/arm_meshes'), glob('urdf/arm_meshes/*.stl')),
-        (os.path.join('lib', package_name), glob('sim_mdrs/udpcanpy.cpython-310-x86_64-linux-gnu.so')),
+        (os.path.join('lib', package_name), glob('sim_mdrs/udpcanpy*')),
         (os.path.join('lib', package_name), glob('sim_mdrs/comms.dbc')),
     ],
     install_requires=['setuptools'],
