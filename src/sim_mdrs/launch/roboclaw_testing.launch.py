04c31c3f19c43784430c953cb3604847cb84c7d2
eseabro <62852911+eseabro@users.noreply.github.com>
2025-01-31 22:31:04 +0100

    added testing launch file

---
+++ src/sim_mdrs/launch/roboclaw_testing.launch.py
@@ -0,0 +1,16 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='sim_mdrs',
+            executable='remote_messages',
+            name='talker_node'
+        ),
+        Node(
+            package='sim_mdrs',
+            executable='roboclaw_node',
+            name='listener_node'
+        )
+    ])
