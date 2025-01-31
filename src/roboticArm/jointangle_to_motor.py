63332d8a1298228b104366aa7b8d833dbdbae1ac
3nricomarinelli <enrico.marinelli2910@gmail.com>
2025-01-31 18:35:19 +0000

    Latest changes from robotic arm branch

--- src/roboticArm/jointangle_to_motor.py
+++ src/roboticArm/jointangle_to_motor.py
@@ -1,57 +1,112 @@
+##Modified code
 import rclpy
+import numpy as np
 from rclpy.node import Node
+from std_msgs.msg import Float32  # Message type for angle input
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
-from adafruit_pca9685 import PCA9685
-from board import SCL, SDA
-import busio
-import RPi.GPIO as GPIO  # Imports the standard Raspberry Pi GPIO library
-from time import sleep   # Imports sleep (aka wait or pause) into the program
-GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
+
+# from adafruit_pca9685 import PCA9685
+# from board import SCL, SDA
+# import busio
+# import argparse
+from adafruit_servokit import ServoKit
+
+# import RPi.GPIO as GPIO  # Imports the standard Raspberry Pi GPIO library
+from time import sleep  # Imports sleep (aka wait or pause) into the program
+
+# GPIO.setmode(GPIO.BOARD) # Sets the pin numbering system to use the physical layout
 
 
 class MotorControlNode(Node):
     def __init__(self):
-        super().__init__('motor_control_node')
+        super().__init__("motor_control_node")
 
-        self.joint_pins = {'joint2': 10, 'joint3': 11, 'joint4': 12, 'joint5': 13} # pinout of GPIOs
+        self.kit = ServoKit(channels=16)
 
-        for joint, pin in self.joint_pins.items():
-            GPIO.setup(pin,GPIO.OUT)
-            self.pin = pin
-            self.p = GPIO.PWM(pin, 50)
-            self.p.start(0) 
+        # Define mapping of joints to PCA9685 channels
+        self.joint_channels = {
+            "joint2": 0,
+            "joint3": 1,
+            "joint4": 2,
+            "joint5": 3,
+        }  # will need to ensure this index  mapping matches the channels - else alter code a bit
+        self.arm_curr_pos = [
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+            0.0,
+        ]  # 1st is imaginary, and there's no gripper here #this should
+        """Check if some channel mapping is required to identify the joints"""
+        sleep(0.1)
+
+        # Initialising some characterisitcs of the motors
+        self.servo_motors = [3]
+        self.cont_motors = [0, 1, 2]
+        self.cont_ang_vel = 360  # deg/s #how do we know this?
+
+        for idx in self.servo_motors:  # to set angle limits on servo motors
+            # check if these intialisation values are correct
+            self.kit.servo[idx].actuation_range = 300
+            self.kit.servo[idx].set_pulse_width_range(500, 2500)
+
+        for idx in self.cont_motors:
+            ## apparently it's optional to set the pulse width range for the continuous servo
+            # self.kit.continuous_servo[idx].set_pulse_width_range(500, 2500)
+            # important thing is to set them all to zero to begin with
+            self.kit.continuous_servo[
+                idx
+            ].throttle = 0  # throttle values go from -1.0 to 1.0
 
         self.subscription = self.create_subscription(
             JointTrajectory,
-            '/arm_controller/joint_trajectory',  # Topic name
+            "/arm_controller/joint_trajectory",  # Topic name
             self.angle_callback,
-            10  # QoS
+            10,  # QoS
         )
-        self.get_logger().info('MotorControlNode started. Listening for angle input on "motor_angle" topic.')
+        self.get_logger().info(
+            'MotorControlNode started. Listening for angle input on "motor_angle" topic.'
+        )
 
     def angle_callback(self, msg):
         """
         Callback for the motor_angle topic. Receives the angle and runs the motor.
         """
-        
+
         # Extract trajectory points
         for joint, point in zip(msg.joint_names, msg.points):
             angle = point.positions
             try:
                 self.run_motor(joint, angle)
-                self.get_logger().info(f'Received angle: {angle}° and applied PWM signal.')
+                self.get_logger().info(
+                    f"Received angle: {angle}° and applied PWM signal."
+                )
             except ValueError as e:
                 self.get_logger().error(str(e))
 
-    def run_motor(self, joint, angle):
+    def run_motor(self, joint, angle):  # assuming this joint is the index not the name
         """
         Convert an angle to PWM and run the motor on the specified channel.
         """
+        # In case input joint is the name and not the index, get index from mapping
+        joint = self.joint_channels[joint]
+
         # Ensure angle is within valid range
-        if angle < 0 or angle > 180:
+        if angle < 0 or angle > 180:  # may not be required
             raise ValueError("Angle must be between 0 and 180 degrees.")
-        
-        self.set_angle(self.joint_pins[joint], angle)
+
+        if joint in self.servo_motors:
+            self.kit.servo[joint].angle = angle
+            self.arm_curr_pos[joint] = angle
+        elif joint in self.cont_motors:
+            ang_diff = angle - self.arm_curr_pos[joint]
+            dt = ang_diff / self.cont_ang_vel
+            self.kit.continuous_servo[joint].throttle = np.sign(ang_diff)*0.5
+            sleep(dt)
+            self.kit.continuous_servo[joint].throttle = 0
+            self.arm_curr_pos[joint] = angle
+
+        # self.set_angle(self.joint_pins[joint], angle)
 
         # Convert angle to PWM signal
         # min_pulse = 1000  # Min pulse length in microseconds
@@ -60,23 +115,13 @@ class MotorControlNode(Node):
         # pulse_length = min_pulse + (angle / 180.0) * pulse_range
         # pwm_value = int(pulse_length / 1000000 * self.pca.frequency * 4096)
 
-
-    def set_angle(self, pin, angle):
-        duty = angle / 18 + 2
-        GPIO.output(pin, True)
-        self.p.ChangeDutyCycle(duty)
-        sleep(1)
-        GPIO.output(pin, False)
-        self.p.ChangeDutyCycle(0) # remove this if motor goes to zero after every angle
-
     def destroy(self):
         """
         Cleanup resources.
         """
         self.pca.deinit()
         super().destroy_node()
-        self.p.stop()
-        GPIO.cleanup()
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -84,11 +129,11 @@ def main(args=None):
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
-        node.get_logger().info('Shutting down.')
+        node.get_logger().info("Shutting down.")
     finally:
         node.destroy()
         rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
